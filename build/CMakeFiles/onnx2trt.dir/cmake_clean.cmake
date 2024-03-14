8f80da9b956ca90563093cd0b26d0080c785a74e
weixiaolin18792820073 <327351669@qq.com>
2024-03-14 11:02:56 +0800

    test-dev

---
+++ build/CMakeFiles/onnx2trt.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/onnx2trt.dir/main1_onnx2trt.cpp.o"
+  "CMakeFiles/onnx2trt.dir/main1_onnx2trt.cpp.o.d"
+  "onnx2trt"
+  "onnx2trt.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/onnx2trt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/trt_infer.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for trt_infer.
+# This may be replaced when dependencies are built.
